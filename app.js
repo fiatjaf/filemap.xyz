48d160bac12237cf0f0b0d1d28bb45ff40bd5944
fiatjaf <fiatjaf@gmail.com>
2017-12-30 22:55:58 +0000

    publish to IPFS instead of uploading to file.io

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-/* global L, atob, btoa, fetch, notie, google, alert */
+/* global L, atob, btoa, fetch, notie, google */
 
 const firebase = require('firebase')
 const GeoFire = require('geofire')
 const places = require('places.js')
+const IPFS = require('ipfs')
+const toBuffer = require('blob-to-buffer')
 const throttle = require('throttleit')
 const SimpleEncryptor = require('simple-encryptor')
 const Dropzone = require('dropzone')
@@ -168,7 +170,8 @@ function attachPlainTextFilesPopup (marker, name, files) {
       <i class="fa ${fileTypeIcon(files[key])}"></i>
       &nbsp;
       <a
-        href="${atob(key).slice(0, 4) === 'http' ? atob(key) : 'https://file.io/' + key}"
+        href="${
+  atob(key).slice(0, 4) === 'http' ? atob(key) : 'https://ipfs.io/ipfs/' + key}"
         target="_blank"
       >${files[key]}</a>
      </li>`
@@ -371,26 +374,101 @@ const updateTargetAddress = throttle(function ({lat, lng}) {
   })
 }, 700)
 
-var keysByDropzoneUUIDs = {}
 var files = {}
-var dz = new Dropzone('#files', {
-  url: 'https://file.io',
-  paramName: 'file',
-  addRemoveLinks: true
-})
-dz.on('removedfile', e => {
-  let removedKey = keysByDropzoneUUIDs[e.upload.uuid]
-  delete files[removedKey]
-})
-dz.on('success', e => {
-  let res = JSON.parse(e.xhr.responseText)
-  if (res.success) {
-    files[res.key] = e.upload.filename
-    keysByDropzoneUUIDs[e.upload.uuid] = res.key
-  } else {
-    alert(e.xhr.responseText)
-    dz.removeFile(e.upload)
+
+class IPFSDropzone extends Dropzone {
+  constructor (el, options) {
+    options.url = 'https://ipfs.io/' // just to bypass the check.
+    super(el, options)
+
+    this.node = null
   }
+
+  uploadFiles (files) {
+    if (this.node === null) {
+      this.node = new Promise((resolve) => {
+        let node = new IPFS({ repo: 'dz-ipfs' })
+        node.once('ready', () => {
+          resolve(node)
+        })
+      })
+    }
+
+    for (let file of files) {
+      this.emit('sending', file)
+    }
+
+    Promise.all([
+      this.node,
+      files,
+      Promise.all(
+        files.map(f =>
+          new Promise((resolve, reject) => {
+            toBuffer(f, (err, buf) => {
+              if (err) return reject(err)
+              resolve(buf)
+            })
+          })
+        )
+      )
+    ])
+      .then(([node, files, buffers]) => {
+        for (let i = 0; i < files.length; i++) {
+          let buf = buffers[i]
+          let file = files[i]
+
+          node.files.add(buf, {
+            progress: loaded => {
+              this._updateFilesUploadProgress([file], null, {
+                loaded: loaded,
+                total: file.size
+              })
+            }
+          }, (err, res) => {
+            let ipfsFile = res[0]
+            file.hash = ipfsFile.hash
+            file.path = ipfsFile.path
+
+            if (err) {
+              this._errorProcessing([file], `ipfs add error: ${err}`)
+              return
+            }
+
+            this._finished([file])
+          })
+        }
+      })
+  }
+}
+
+var dz = new IPFSDropzone('#files', {
+  addRemoveLinks: true,
+  accept: (file, done) => {
+    if (file.size > 50000000) {
+      done('Files must be smaller than 50MB.')
+    } else {
+      done()
+    }
+  }
+})
+dz.on('removedfile', file => {
+  delete files[file.hash]
+})
+dz.on('success', file => {
+  console.log('published to IPFS:', file)
+  if (file.size > 50000000) {
+    return
+  }
+
+  files[file.hash] = file.name
+})
+dz.on('error', (file, message) => {
+  console.error(message, file)
+  notie.alert({
+    type: 'error',
+    text: message
+  })
+  dz.removeFile(file)
 })
 
 var links = []
